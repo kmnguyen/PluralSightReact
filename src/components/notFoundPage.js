0f70b75a161e5b47c58da7e2e04b5c36edebc149
Khoi <khoi.nguyen@am.sony.com>
2016-01-16 11:19:58 -0800

    Initial commit

---
+++ src/components/notFoundPage.js
@@ -0,0 +1,18 @@
+'user strict';
+
+var React = require('react');
+var Link = require('react-router').Link;
+
+var NotFoundPage = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h1>Page Not Found</h1>
+				<p>Whoops! Sorry, nothing here</p>
+				<p><Link to='app'>Back to Home</Link></p>
+			</div>
+		);
+	}
+});
+
+module.exports = NotFoundPage;
