f8b0cdb1a786be543ca4572b7409cdaf8e8cc26c
Khoi <khoi.nguyen@am.sony.com>
2016-01-23 02:48:15 -0800

    Consume textInput.js in author form

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -1,35 +1,25 @@
 'use strict';
 
 var React = require('react');
+var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
     render: function(){
         return (
             <form>
                 <h1>Manage Author</h1>
-                <label htmlFor="firstName">First Name</label>
 
-                <input type="text"
-                       name="firstName"
-                       className="form-control"
-                       placeholder="First Name"
-                       ref="firstName"
+                <Input name="firstName"
+                       label="First Name"
                        onChange={this.props.onChange}
                        value={this.props.author.firstName}
                 />
-                <br />
 
-                <label htmlFor="lastName">Last Name</label>
-
-                <input type="text"
-                       name="lastName"
-                       className="form-control"
-                       placeholder="Last Name"
-                       ref="lastName"
+                <Input name="firstName"
+                       label="First Name"
                        onChange={this.props.onChange}
-                       value={this.props.author.lastName}
+                       value={this.props.author.firstName}
                 />
-                <br />
 
                 <input type="submit" value="Save" className="btn btn-default" />
             </form>
